c2a6ac0279b2079df0873e2779e2f53405ce37fa
Niklas Dörfler <niklas.doerfler@krone.de>
2024-06-18 09:14:55 +0200

    feat: add additional endpoints option

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This incredibly simple script provides a HTTP server used to forward the request to one or more servers. For this you need to provide a DNS name in the env variable `TARGET_HOSTS_DNS_NAME`. For each ip address to which this hostname resolves, this script issues a request on the port defined in `TARGET_HOSTS_PORT` and the given method and path from the original request.
 
 Use case for this is to forward a http trigger to all instances of a deployment in a kubernetes cluster in parallel. The idea is to use a headless service for this case to retrieve all pods ip addresses by querying the hostname of this headless service.
+
+Additionally, you can specify the env variable `TARGET_ADDITIONAL_FORWARD_ENDPOINTS` to add additional endpoints, to which the request should be forwarded. It accepts a comma separated list of URL and method combinations like `http://your-endpoint:1234/test;POST`.
+
--- app.js
+++ app.js
@@ -1,36 +1,58 @@
 const express = require('express');
-const http = require("http");
-
+const http = require('http');
 const dns = require('dns');
+
 const app = express();
 const port = 3000;
 
+const forwardEndpoints = process.env.TARGET_ADDITIONAL_FORWARD_ENDPOINTS || '';
+
+const parseEndpoints = (endpointsStr) => {
+    const endpoints = [];
+    if (endpointsStr) {
+        endpointsStr.split(',').forEach((ep) => {
+            const lastSemicolonIndex = ep.lastIndexOf(';');
+            const url = ep.substring(0, lastSemicolonIndex);
+            const method = ep.substring(lastSemicolonIndex + 1).toUpperCase();
+            const parsedUrl = new URL(url);
+            endpoints.push({
+                hostname: parsedUrl.hostname,
+                port: parsedUrl.port || 80,
+                path: parsedUrl.pathname + parsedUrl.search,
+                method,
+            });
+        });
+    }
+    return endpoints;
+};
+
+const endpointsList = parseEndpoints(forwardEndpoints);
 
 app.get('/health', (req, res, next) => {
     const target = process.env.TARGET_HOSTS_DNS_NAME;
-    if (target == undefined) {
-        return next(new Error("Environment variable TARGET_HOSTS_DNS_NAME is not set"))
+    if (target === undefined) {
+        return next(new Error("Environment variable TARGET_HOSTS_DNS_NAME is not set"));
     }
 
     dns.lookup(target, { all: true }, (err, addresses) => {
-        if (addresses == undefined) {
+        if (addresses === undefined) {
             return next(new Error("Unable to resolve hostname '" + target + "'."));
         }
-        res.send(addresses)
-    })
-})
+        res.send(addresses);
+    });
+});
 
 app.all('*', (req, res, next) => {
     dns.lookup(process.env.TARGET_HOSTS_DNS_NAME, { all: true }, (err, addresses) => {
-        if (addresses == undefined) {
+        if (addresses === undefined) {
             return next(new Error("Unable to resolve hostname '" + process.env.TARGET_HOSTS_DNS_NAME + "'."));
         }
 
         const promises = [];
 
-        addresses.forEach(address => {
-            promises.push(new Promise((res, rej) => {
-                if (req.headers.host != undefined) {
+        addresses.forEach((address) => {
+            promises.push(new Promise((resolve, reject) => {
+                if (req.headers.host !== undefined) {
                     delete req.headers.host;
                 }
                 const options = {
@@ -38,15 +60,22 @@ app.all('*', (req, res, next) => {
                     port: process.env.TARGET_HOSTS_PORT,
                     path: req.url,
                     method: req.method,
-                    headers: req.headers
-                }
+                    headers: req.headers,
+                };
 
-                console.log("Sending %s request to http://%s:%d%s with headers: %s", options.method, options.hostname, options.port, options.path, JSON.stringify(options.headers));
+                console.log(
+                    "Sending %s request to http://%s:%d%s with headers: %s",
+                    options.method,
+                    options.hostname,
+                    options.port,
+                    options.path,
+                    JSON.stringify(options.headers)
+                );
 
                 http
-                    .request(options, resp => {
+                    .request(options, (resp) => {
                         const chunks = [];
-                        resp.on('data', data => chunks.push(data));
+                        resp.on('data', (data) => chunks.push(data));
                         resp.on('end', () => {
                             let resBody = Buffer.concat(chunks);
                             switch (resp.headers['content-type']) {
@@ -54,37 +83,104 @@ app.all('*', (req, res, next) => {
                                     resBody = JSON.parse(resBody);
                                     break;
                             }
-                            console.log("Got response for call: http://%s:%d%s -> %d: %s", options.hostname, options.port, options.path, resp.statusCode, resp.statusMessage);
-                            res({
+                            console.log(
+                                "Got response for call: http://%s:%d%s -> %d: %s",
+                                options.hostname,
+                                options.port,
+                                options.path,
+                                resp.statusCode,
+                                resp.statusMessage
+                            );
+                            resolve({
                                 ...options,
                                 statusCode: resp.statusCode,
                                 statusMessage: resp.statusMessage,
-                                body: resBody.toString()
-                            })
-
-                        })
+                                body: resBody.toString(),
+                            });
+                        });
                     })
-                    .on("error", err => {
-                        console.log("Error: " + err.message);
-                        // Promise.all does not work with errors
-                        res({
+                    .on('error', (err) => {
+                        console.log('Error: ' + err.message);
+                        resolve({
                             ...options,
-                            err: err
-                        })
+                            err: err,
+                        });
                     })
                     .end();
             }));
         });
 
-        Promise
-            .all(promises)
+        endpointsList.forEach((endpoint) => {
+            promises.push(new Promise((resolve, reject) => {
+                const options = {
+                    hostname: endpoint.hostname,
+                    port: endpoint.port,
+                    path: endpoint.path,
+                    method: endpoint.method,
+                    headers: req.headers,
+                    insecureHTTPParser: true
+                };
+
+                console.log(
+                    "Sending %s request to http://%s:%d%s with headers: %s",
+                    options.method,
+                    options.hostname,
+                    options.port,
+                    options.path,
+                    JSON.stringify(options.headers)
+                );
+
+                const reqForward = http.request(options, (resp) => {
+                    const chunks = [];
+                    resp.on('data', (data) => chunks.push(data));
+                    resp.on('end', () => {
+                        let resBody = Buffer.concat(chunks);
+                        switch (resp.headers['content-type']) {
+                            case 'application/json':
+                                resBody = JSON.parse(resBody);
+                                break;
+                        }
+                        console.log(
+                            "Got response for call: http://%s:%d%s -> %d: %s",
+                            options.hostname,
+                            options.port,
+                            options.path,
+                            resp.statusCode,
+                            resp.statusMessage
+                        );
+                        resolve({
+                            ...options,
+                            statusCode: resp.statusCode,
+                            statusMessage: resp.statusMessage,
+                            body: resBody.toString(),
+                        });
+                    });
+                });
+
+                reqForward.on('error', (err) => {
+                    console.log('Error: ' + err.message);
+                    resolve({
+                        ...options,
+                        err: err,
+                    });
+                });
+
+                if (req.body) {
+                    reqForward.write(JSON.stringify(req.body));
+                }
+                reqForward.end();
+            }));
+        });
+
+        Promise.all(promises)
             .then((data) => {
-                console.log(data)
+                console.log(data);
                 res.send(data);
-            }).catch(err => res.status(400).send(err));
+            })
+            .catch((err) => res.status(400).send(err));
     });
-})
+});
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Listening on port ${port}...`)
-})
+    console.log(`Listening on port ${port}...`);
+});
